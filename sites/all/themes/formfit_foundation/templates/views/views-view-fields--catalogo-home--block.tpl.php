72a0f3ee28753f2844991cb4adaa186a005e12d0
Edwin Hernández Cabrera <edinfamous@gmail.com>
2015-06-10 13:44:05 -0500

    Cambios en estilos custom

--- sites/all/themes/formfit_foundation/templates/views/views-view-fields--catalogo-home--block.tpl.php
+++ sites/all/themes/formfit_foundation/templates/views/views-view-fields--catalogo-home--block.tpl.php
@@ -1,5 +1,9 @@
-<div>
+<div class="image-hover">
 <?php print $fields['field_image']->content;?>
+  <div class="large-12 columns none">
+    <div class="large-5 columns ver"><?php print $fields['view_node']->content;?></div>
+    <div class="large-7 columns comparar"><?php print $fields['nothing']->content;?></div>
+  </div>
   <div class="content-car-title">
     <span class="large-10 columns"><?php print $fields['title']->content;?></span>
     <span class="large-2 columns"><?php print $fields['add_to_cart_form']->content;?></span>
