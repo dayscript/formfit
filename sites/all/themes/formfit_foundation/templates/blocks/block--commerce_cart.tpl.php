5febfc159e2a9d97b14aaa1cd9cba96ac9986de3
Edwin Hernández Cabrera <edinfamous@gmail.com>
2015-07-29 00:49:01 -0500

    Se elimina mensaje cuando no hay articulos agregados

--- sites/all/themes/formfit_foundation/templates/blocks/block--commerce_cart.tpl.php
+++ sites/all/themes/formfit_foundation/templates/blocks/block--commerce_cart.tpl.php
@@ -1,5 +1,5 @@
 <section class="large-1 columns">
-  <a aria-expanded="false" aria-controls="car-sale" data-dropdown="car-sale" class="button car-menu"><i class="fi-shopping-cart"></i> <?php print render($block->subject);?></a>
+  <a aria-expanded="false" aria-controls="car-sale" data-dropdown="car-sale" class="button car-menu"><i class="fi-shopping-cart"></i> <!--<?php print render($block->subject);?>--></a>
 </section>
 <ul aria-autoclose="false" aria-hidden="true" tabindex="-1" data-dropdown-content="" class="medium f-dropdown" id="car-sale" style="position: absolute; left: -99999px; top: 48px;">
     <p class="line-car"></p>
